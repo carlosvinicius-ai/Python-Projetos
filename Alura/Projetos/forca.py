d8436264f9457c8b3e989b0b098992a9420fa841
cvinicius-ai <cvinicius32@hotmail.com>
2022-06-08 20:21:38 -0300

    adicionando o tipo bool

--- Alura/Projetos/forca.py
+++ Alura/Projetos/forca.py
@@ -1,9 +1,17 @@
 
 def jogar():
     
-    print("{:=<30}".format('='))
-    print("Bem-vindo(a) ao jogo da Forca")
-    print("{:=<30}".format('='))
+    print('{:=<30}'.format('='))
+    print('Bem-vindo(a) ao jogo da Forca')
+    print('{:=<30}'.format('='))
+    
+    palavra_secreta = 'lindo'
+    enforcou = False #bool
+    acertou = False
+    
+    #Enquanto não enforcou e não acertou contiua
+    while(not enforcou and not acertou):     
+        print('jogando')
     
 if(__name__ == '__main__'):     #ele seta automaticamente uma variavel e isso é para fazer a verificação
     jogar()
