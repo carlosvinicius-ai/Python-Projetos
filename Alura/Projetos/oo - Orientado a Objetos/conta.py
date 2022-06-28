618bff3bd3a634b206966f738da6883eee385ba4
cvinicius-ai <cvinicius32@hotmail.com>
2022-06-28 20:36:36 -0300

    aprendendo sobre encapsulamento

--- Alura/Projetos/oo - Orientado a Objetos/conta.py
+++ Alura/Projetos/oo - Orientado a Objetos/conta.py
@@ -2,16 +2,16 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):     # __init__ é uma função contrutora
         print("Construindo objeto...")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
