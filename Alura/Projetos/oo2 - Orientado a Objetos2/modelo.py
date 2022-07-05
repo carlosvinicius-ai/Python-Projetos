bea9d9eb955860c13836481241cc0da485a02a58
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-05 20:57:57 -0300

    Criando o metodo likes

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -1,17 +1,27 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+        
+    def dar_like(self):
+        self.likes += 1
         
 class Serie:
     def __init__(self, nome, ano, temporada):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporada = temporada
+        self.likes = 0
+        
+    def dar_like(self):
+        self.likes += 1
 
-vingadores = Filme("Vingadores", 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}m')
+
+
+vingadores = Filme("vingadores - guerra infinita", 2018, 160)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}m - Likes: {vingadores.likes}')
 
 atlanta = Serie("atlanta", 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporada}')
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporada} - Likes {atlanta.likes}')
