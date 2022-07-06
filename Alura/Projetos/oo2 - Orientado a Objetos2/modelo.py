8c1f5cec5fdae559d02bd7f4a27a22d552409f8e
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-05 21:06:42 -0300

    adicionando getter e setter

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -1,19 +1,59 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
-        self.duracao = duracao
-        self.likes = 0
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__duracao = duracao
+        self.__likes = 0
         
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+        
+    @property
+    def ano(self):
+        return self.__ano
+    
+    @property
+    def duracao(self):
+        return self.__duracao
+        
+    @property
+    def likes(self):
+        return self.__likes
+    
     def dar_like(self):
-        self.likes += 1
+        self.__likes += 1
         
 class Serie:
     def __init__(self, nome, ano, temporada):
-        self.nome = nome.title()
-        self.ano = ano
-        self.temporada = temporada
-        self.likes = 0
+        self.__nome = nome.title()
+        self.__ano = ano
+        self.__temporada = temporada
+        self.__likes = 0
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+        
+    @property
+    def ano(self):
+        return self.__ano
+    
+    @property
+    def temporada(self):
+        return self.__temporada
+        
+    @property
+    def likes(self):
+        return self.__likes
         
     def dar_like(self):
         self.likes += 1
