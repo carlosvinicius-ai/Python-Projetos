04e02fad28fd1e2e8d000ec7382a43efab62de07
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-07 20:24:56 -0300

    aprendendo dobre a funcao __str__

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -19,16 +19,24 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):      # dunder methods - neste caso um metodo criado para representar o objeto da classe de forma textual
+        return f'Nome: {self._nome} - Ano: {self.ano} - Likes {self._likes}'
 
 class Filme (Programa):         #para criar uma herança com a classe mãe
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao  
         
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Duração: {self.duracao} min - Likes {self._likes}'
+        
 class Serie (Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
+        
+    def __str__(self):
+        return f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporada} - Likes {self._likes}'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -40,11 +48,7 @@ vingadores.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}m - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporada} - Likes {atlanta.likes}')
-
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporada     #hasattr é utilizado para saber se possui o atributo selecionado
-    print(f'Nome: {programa.nome} - Ano: {programa.ano} - Temporadas/Duração: {detalhes} - Likes {programa.likes}')
+    print(programa)
