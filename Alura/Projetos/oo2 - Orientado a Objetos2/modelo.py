bca4d04a0ce4d24fb30c0db2be93f5c7af59dee1
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-11 20:26:39 -0300

    Criando a classe Playlist

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -38,17 +38,30 @@ class Serie (Programa):
     def __str__(self):
         return f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporada} - Likes {self._likes}'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo Mundo em Panico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
 vingadores.dar_like()
 vingadores.dar_like()
 vingadores.dar_like()
+demolidor.dar_like()
+tmep.dar_like()
+tmep.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana.programas:
     print(programa)
