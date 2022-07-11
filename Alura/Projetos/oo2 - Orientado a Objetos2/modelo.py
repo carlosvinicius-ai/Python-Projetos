365e4bddbce72ce0945ff29bb8c34139dd7d6c5b
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-11 20:34:27 -0300

    Aprendendo sobre o metodo list

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -38,13 +38,11 @@ class Serie (Programa):
     def __str__(self):
         return f'Nome: {self._nome} - Ano: {self.ano} - Temporadas: {self.temporada} - Likes {self._likes}'
 
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
         self.nome = nome
-        self.programas = programas
+        super().__init__(programas)
 
-    def tamanho(self):
-        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -63,5 +61,7 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in playlist_fim_de_semana.programas:
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
