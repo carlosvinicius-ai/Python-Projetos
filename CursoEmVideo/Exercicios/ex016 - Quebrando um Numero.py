13b0226638d96ff64299ee3f46764b0fd649b296
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-14 20:14:34 -0300

    Aprendendo outros metodos de arredondamento

--- CursoEmVideo/Exercicios/ex016 - Quebrando um Numero.py
+++ CursoEmVideo/Exercicios/ex016 - Quebrando um Numero.py
@@ -1,11 +1,22 @@
 # Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 
+
+# utilizando a biblioteca math
+
 import math
 
 print('='*50)
 print('Exercício 16')
 print('='*50)
 
-num = float(input('Digite um numero qualquer: '))
-inteiro = math.ceil(num)    #utilizado para arredondar
-print(inteiro)
+num = float(input('Digite um valor: '))
+
+print(f'O valor real é {num} e sua porção inteira é {math.trunc(num)}') #utilizado para arredondar
+
+#match.ceil() é utilizado para arredondar para cima
+
+#sem utilizar nenhuma biblioteca
+
+num = float(input('\nDigite um valor: '))
+
+print(f'O valor real é {num} e sua porção inteira é {int(num)}')
