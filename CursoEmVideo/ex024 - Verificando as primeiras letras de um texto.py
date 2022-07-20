260a01ff73fb98d4195017185134b6bffaf2ea9c
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-20 20:18:37 -0300

    atualizando apresentação ex24

--- CursoEmVideo/ex024 - Verificando as primeiras letras de um texto.py
+++ CursoEmVideo/ex024 - Verificando as primeiras letras de um texto.py
@@ -1,5 +1,9 @@
 # Exercício Python 24: Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.
 
+print('='*50)
+print('Exercício 24 – Verificando as primeiras letras de um texto')
+print('='*50)
+
 cid = str(input('digite o nome de uma cidade: ')).strip().upper()
 
 print(cid[:5] == 'SANTO')
---
+++ CursoEmVideo/ex025 - Procurando uma string dentro de outra.py
@@ -0,0 +1,2 @@
+# Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+
