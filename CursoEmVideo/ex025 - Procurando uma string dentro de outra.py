a8129ab87366c75327a3a1ba68050f67938c29b8
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-20 20:28:39 -0300

    Exercicio 26 arquivo

--- CursoEmVideo/ex025 - Procurando uma string dentro de outra.py
+++ CursoEmVideo/ex025 - Procurando uma string dentro de outra.py
@@ -1,2 +1,9 @@
 # Exercício Python 25: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
 
+print('='*50)
+print('Exercício 24 – Exercício 25 – Procurando uma string dentro de outra')
+print('='*50)
+
+nome = str(input('Digite seu nome completo: ')).strip().upper()
+
+print('Seu nome tem Silva?' .format({'SILVA' in nome}))
