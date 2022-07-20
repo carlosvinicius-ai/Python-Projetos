06cfb345406e49e1466b52c4e027d70758ba63d6
cvinicius-ai <cvinicius32@hotmail.com>
2022-07-20 19:43:12 -0300

    ex22

--- Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
+++ Alura/Projetos/oo2 - Orientado a Objetos2/modelo.py
@@ -54,6 +54,7 @@ vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 tmep = Filme('Todo Mundo em Panico', 1999, 100)
 demolidor = Serie('Demolidor', 2016, 2)
+gilmore = Serie('Gilmore', 2000, 5)
 
 vingadores.dar_like()
 vingadores.dar_like()
@@ -64,7 +65,7 @@ tmep.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep, gilmore]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
 print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
---
+++ CursoEmVideo/Exercicios/ex022 - Analisador de Textos.py
@@ -0,0 +1,22 @@
+'''Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+
+– O nome com todas as letras maiúsculas e minúsculas.
+
+– Quantas letras ao todo (sem considerar espaços).
+
+– Quantas letras tem o primeiro nome.'''
+
+
+print('='*50)
+print('Exercício 22 – Analisador de Textos')
+print('='*50)
+
+nome = input('\nDigite seu nome completo: ').strip()
+separa = nome.split()
+
+print(f'''Seu nome com todas letras maiuscula é: {nome.upper()}
+Seu nome com todas as letras minuscula é: {nome.lower()}
+Seu nome tem: {len(nome)- nome.count(' ')} letras
+Seu primeiro nome é {separa[1]} tem {len(separa[0])} letras''')
+
+# - nome.count() para diminuir
