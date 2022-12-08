65c32b38a1ef4fecab0b756b3585aad6c654900c
cvinicius-ai <cvinicius32@hotmail.com>
2022-12-08 20:53:10 -0300

    performing tasks

---
+++ Coursera/Introdução à Ciência da Computação com Python Parte 1/tarefas/quadrado.py
@@ -0,0 +1,6 @@
+quadrado = float(input('Digite o valor correspondente ao lado de um quadrado: '))
+
+perimetro = quadrado * 4
+area = quadrado ** 2
+
+print('perímetro:', perimetro, '- área:', area)
