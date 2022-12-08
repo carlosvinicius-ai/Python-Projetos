04d2297921374ae65c9dd38781f68776fe11c898
cvinicius-ai <cvinicius32@hotmail.com>
2022-12-08 20:24:29 -0300

    add file Temperatura and BomDia

---
+++ Coursera/Introdução à Ciência da Computação com Python Parte 1/BomDia.py
@@ -0,0 +1,4 @@
+nomeDaMae = input('Digite o nome da sua mãe: ')
+nomeDoPai = input('Digite o nome do seu pai: ')
+
+print('Bom dia Sra.', nomeDaMae, '!!! e Bom dia Sr.', nomeDoPai, '!!!')
---
+++ Coursera/Introdução à Ciência da Computação com Python Parte 1/ConversorDeTemperatura.py
@@ -0,0 +1,5 @@
+F = float(input("Digite a temperatura em Fahrenheit: "))
+
+Celsius = (F - 32) * 5 / 9
+
+print("A temperuatura em Celsius é ", Celsius)
