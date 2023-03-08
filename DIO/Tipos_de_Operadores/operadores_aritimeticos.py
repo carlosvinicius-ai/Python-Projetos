037f7d54d42d5777b73c1ec354689c07fdc21b0e
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-08 20:34:52 -0300

    diferentes tipos de operadores

---
+++ DIO/Tipos_de_Operadores/operadores_aritimeticos.py
@@ -0,0 +1,29 @@
+produto_1 = 10
+produto_2 = 20
+
+# Adição 
+print(produto_1 + produto_2 + 3.5)
+
+# Subtração
+
+print(produto_1 - produto_2)
+
+# Divisão
+
+print(produto_1 / produto_2)
+
+#Divisão por inteiro
+
+print(produto_1 // produto_2)
+
+#Multiplicação
+
+print(produto_1 * produto_2)
+
+# Modulo
+
+print(produto_1 % produto_2)
+
+# Exponenciação
+
+print(produto_1 ** produto_2)
