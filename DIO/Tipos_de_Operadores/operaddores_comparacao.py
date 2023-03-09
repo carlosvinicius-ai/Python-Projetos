1cd60ec1d2732ac82f2774b831ed1d60be334769
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-09 19:59:03 -0300

    add operadores de comparacao

---
+++ DIO/Tipos_de_Operadores/operaddores_comparacao.py
@@ -0,0 +1,16 @@
+saldo = 200
+saque = 200
+
+
+# comparação
+print(saque == saldo)
+# diferente
+print(saque != saldo)
+#maior
+print(saque > saldo)
+#maior ou igual
+print(saque >= saldo)
+#menor
+print(saque < saldo)
+#menor ou igual
+print(saque <= saldo)
