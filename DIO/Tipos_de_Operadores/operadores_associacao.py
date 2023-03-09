13f27d92aea5f45653cc3bd69a1139dc60dc042d
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-09 20:03:02 -0300

    operadores de associacao

---
+++ DIO/Tipos_de_Operadores/operadores_associacao.py
@@ -0,0 +1,9 @@
+# IN = para verificar se está na sequência
+# NOT IN = para verificar se não está na sequência
+
+frutas = ['limao', 'uva', 'laranja']
+curso = 'Curso de Python'
+
+print('laranja' in frutas)
+print('laranja' not in frutas)
+print('Python' in curso)
