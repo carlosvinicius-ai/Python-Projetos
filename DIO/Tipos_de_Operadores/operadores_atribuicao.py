73e2459d341829fc00934bb75f4bc5092738164f
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-09 19:59:53 -0300

    operadores de atribuicao

---
+++ DIO/Tipos_de_Operadores/operadores_atribuicao.py
@@ -0,0 +1,23 @@
+#Atribuicao Simples
+saldo = 500
+print(saldo)
+
+#Atribuição Soma
+saldo += 10
+print(saldo)
+
+#Atribuição Subtração
+saldo -= 5
+print(saldo)
+
+#Atribuição Multiplicação
+saldo *= 20
+print(saldo)
+
+#Atribuição Divisão
+saldo /= 2
+print(saldo)
+
+#Atribuição Divisão por inteiro
+saldo //= 6
+print(saldo)
