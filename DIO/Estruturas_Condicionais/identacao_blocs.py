a36d866e40f004555e4e7399c564c6c88726c50b
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-09 21:31:45 -0300

    entendendo identacao de blocos

---
+++ DIO/Estruturas_Condicionais/identacao_blocs.py
@@ -0,0 +1,10 @@
+def sacar(valor):
+    saldo = 1000
+    if saldo >= valor:
+        print('valor sacado')
+        print('retire o seu dinheiro')
+        
+    print('Obrigado por ser nosso cliente!')
+        
+        
+sacar(100)
