2d024de5aa22618120f8003777716cd3053bf4bd
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-13 20:39:05 -0300

    range in python

--- DIO/Estruturas_Condicionais/estrutura_repeticao_for.py
+++ DIO/Estruturas_Condicionais/estrutura_repeticao_for.py
@@ -1,8 +1,14 @@
 texto = input('Informe o texto: ')
 vogais = 'AEUIOU'
 
+#Exemplo utilizando a funcao built-in range
 for letra in texto:
     if letra.upper() in vogais:
         print(letra, end=' ')
 else:
     print()
+    
+
+#Exemplo com range
+for numero in range(0, 51, 5):
+    print(numero, end= ' ')
