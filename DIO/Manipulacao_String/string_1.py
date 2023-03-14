6fa2940689feb773eaabcdfb6083c2f328de8713
cvinicius-ai <cvinicius32@hotmail.com>
2023-03-14 18:13:02 -0300

    add string_1

---
+++ DIO/Manipulacao_String/string_1.py
@@ -0,0 +1,15 @@
+nome = 'CaRlos'
+
+print(nome.upper()) #para deixar em caixa alta
+print(nome.lower()) #deixar em caixa baixa
+print(nome.title()) #deixar a primeira letra em caixa alta
+
+texto = '   Ola Mundo   '
+print(texto)
+print(texto.strip()) #retirar o espaço
+print(texto.lstrip()) #retirar o espaço a esquerda
+print(texto.rstrip()) #reitrar o espaço a direito
+
+menu = 'Python'
+print(menu.center(14, '#')) #centralizar o texto
+print("-".join(menu)) # adicionar uma informação a cadas letra do texto
