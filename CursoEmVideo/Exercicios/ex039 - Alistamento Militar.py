ab013701acac3d9a6426a9a6bf400a84606ea151
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-12 18:40:10 -0300

    resolvendo ex39

--- CursoEmVideo/Exercicios/ex039 - Alistamento Militar.py
+++ CursoEmVideo/Exercicios/ex039 - Alistamento Militar.py
@@ -1,1 +1,14 @@
 # Faça um programa que leia o ano de nascimento de um jovem e informe de acordo com sua idade: - Se ele ainda vai se alistar, - Se é a hora de se alistar, - Se ja passou da hora de se alistar. Seu programa também deve mostrar o tempo que falta ou que passou do prazo
+
+from datetime import datetime
+
+ano_nascimento = int(input('Informe seu ano de nascimento: '))
+
+idade = 2023 - ano_nascimento
+
+if idade < 18:
+    print('Você ainda vai se alistar, falta {} anos'.format(18 - idade))
+elif idade > 18:
+    print('Ja passou o da hora de se alistar, passou {} anos'.format(idade - 18))
+else:
+    print('Está na hora de se alistar')
