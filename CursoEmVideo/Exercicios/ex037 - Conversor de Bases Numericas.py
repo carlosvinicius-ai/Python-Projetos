7560d4029d8d9d8cdc4f5399d91f009f021b23f6
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-12 17:12:19 -0300

    resolvendo ex37

--- CursoEmVideo/Exercicios/ex037 - Conversor de Bases Numericas.py
+++ CursoEmVideo/Exercicios/ex037 - Conversor de Bases Numericas.py
@@ -1,1 +1,13 @@
 # Escreva um programa que leia um número inteiro qualquer e peça para o usuário escolher a base de conversão -  1: binario 2: octal 3: hexadecimal
+
+numero = int(input('Escreva um número inteiro: '))
+conversao = int(input('Escolha a base de conversão: \n1 - BINÁRIO \n2 - OCTAL \n3- HEXADECIMAL \nSua opção: '))
+
+if conversao == 1:
+    print('{} convertido para BINÁRIO é igual a {}'.format(numero, bin(numero)[2:]))
+elif conversao == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(numero, oct(numero)[2:]))
+elif conversao == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(numero, hex(numero)[2:]))
+else:
+    print('Por favor insira uma opção válida')
