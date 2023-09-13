c5310a0e3a23433e8206374c4bd8653f2902615b
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-13 12:32:11 -0300

    resolvendo ex42

--- CursoEmVideo/Exercicios/ex042 - Analisando Triangulos v2.0.py
+++ CursoEmVideo/Exercicios/ex042 - Analisando Triangulos v2.0.py
@@ -1,1 +1,16 @@
 # Refaça o desafio 32 dos triangulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: - Equilatero: Todos os lados iguais, - Isósceles: dois lados iguais, - Escaleno: todos os lados diferentes
+
+a = float(input('Digite a primeira reta: '))
+b = float(input('Digite a segunda reta: '))
+c = float(input('Digite a terceira reta: '))
+
+if (a < b + c) and (b < c + a) and (c < b + a):
+    print('É um triangulo = ', end='')
+    if a == b == c:
+        print('EQUILÁTERO!!')
+    elif a != b != c != a:
+        print('ESCALENO!')
+    else:
+        print('ISÓCELES')
+else:
+    print('Não é um triângulo')
