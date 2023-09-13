63ace92728901bec1742f40371652f93010de1ab
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-13 15:32:58 -0300

    melhorando ex44

--- CursoEmVideo/Exercicios/ex044 - Gerenciador de Pagamentos.py
+++ CursoEmVideo/Exercicios/ex044 - Gerenciador de Pagamentos.py
@@ -1,5 +1,6 @@
 # Elabore um programa que calcule o valor a ser pago por um produto, considerando seu preço normal e condição de pagamento: - Avista dinheiro: 10% de desconto, - Avista no cartão: 5% de desconto, até 2x no cartão preço normal, - 3x ou mais no cartão: 20% de juros
 
+print('{:=^40}'.format(' LOJAS CARLIN '))
 valor_produto = float(input('Digite o valor do produto: R$'))
 print('''
 Escolha uma das opções:
@@ -10,18 +11,21 @@ Escolha uma das opções:
 ''')
 opcao = int(input('Opção: '))
 
-print('O produto sairá no valor de ', end='')
-
 if opcao == 1:
     valor = valor_produto * 0.9
-    print('R${:.2f}'.format(valor))
 elif opcao == 2:
     valor = valor_produto * 0.95
-    print('R${:.2f}'.format(valor))
 elif opcao == 3:
-    print(print('R${:.2f}'.format(valor_produto)))
+    valor = valor_produto
+    parcela = valor / 2
+    print('Sua compra será em 2x com cada parcela no valor R${:.2f} SEM JUROS'.format(parcela))
 elif opcao == 4:
     valor = valor_produto * 1.20
-    print('R${:.2f}'.format(valor))
+    numero_parcela = int(input('Digite o número de parcelas: '))
+    parcela = valor / numero_parcela
+    print('Sua compra será em {}x com cada parcela no valor R${:.2f} COM JUROS.'.format(numero_parcela, parcela))
 else:
-    print('R$ invalido')
+    valor = 0
+    print('Digite um valor válido')
+
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(valor_produto, valor))
