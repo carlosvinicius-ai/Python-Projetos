5f6c4d3f28f8b2284d1abbf0e7fbda8ff4a3bdae
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 15:03:49 -0300

    resolvendo ex050

--- CursoEmVideo/Exercicios/ex050 - Soma dos Pares.py
+++ CursoEmVideo/Exercicios/ex050 - Soma dos Pares.py
@@ -1,1 +1,12 @@
 # Desenvolva um programa que leia 6 números inteiros e mostre apenas a soma daqueles que forem pares. Se o valor for impar, desconsidere-o
+
+soma = 0
+pares = []
+
+for i in range (1, 7):
+    num = int(input('Digite o {}º número: '.format(i)))
+    if num % 2 == 0:
+        soma += num
+        pares.append(num)
+
+print('A soma de todos valores pares que são {} é igual a {}'.format(pares, soma))
