6b6bf1268d3c55d489fac5e63859c794f198834c
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 15:44:12 -0300

    resolvendo ex051

--- CursoEmVideo/Exercicios/ex051 - Progressao Aritmetica.py
+++ CursoEmVideo/Exercicios/ex051 - Progressao Aritmetica.py
@@ -1,1 +1,10 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de um PA. No final, mostre os 10 primeiros termos dessa progressão
+
+primeiro = int(input('Primeiro termo: '))
+razao = int (input('Razão: '))
+
+decimo = primeiro + (10 - 1) * razao
+
+for i in range(primeiro, decimo + razao, razao):
+    print('{}'.format(i), end=' -> ')
+print('ACABOU')
