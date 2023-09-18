0422b3eb874240fa61fb0d2264c24d3bbe2d3250
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 18:12:55 -0300

    resolvendo ex053

--- CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
+++ CursoEmVideo/Exercicios/ex053 - Detector de Palindromo.py
@@ -1,1 +1,16 @@
 # Crie um programa que leia uma frase qualquer e diga se ela é um polindromo, desconsiderando os espaços
+
+frase = input('Digite uma frase: ').strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+    
+print('O inverso de {} é {}'.format(junto, inverso))
+
+if inverso == junto:
+    print('É um PALINDROMO')
+else:
+    print('Não é um PALINDROMO')
