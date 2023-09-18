bbc5ee02f3018e9a645058b2f7f8f7d7a3b4d028
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 17:54:01 -0300

    resolvendo ex052

--- CursoEmVideo/Exercicios/ex052 - Numeros primos.py
+++ CursoEmVideo/Exercicios/ex052 - Numeros primos.py
@@ -1,1 +1,18 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo
+
+num = int(input('Digite um número: '))
+total = 0
+
+for i in range (1, num + 1):
+    if num % i == 0:
+        print('\033[32m', end=' ')
+        total += 1
+    else:
+        print('\033[31m', end=' ')
+    print(i, end= ' ')
+print('\nO número {} foi divisivel {} vezes' .format(num, total))
+
+if total == 2:
+    print('Por isso ele é PRIMO')
+else:
+    print('E por isso ele NÃO É PRIMO')
