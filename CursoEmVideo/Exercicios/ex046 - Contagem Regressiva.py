2112985c27436f9d080788b3b6e722cdd1812070
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 11:59:38 -0300

    resolvendo ex046

--- CursoEmVideo/Exercicios/ex046 - Contagem Regressiva.py
+++ CursoEmVideo/Exercicios/ex046 - Contagem Regressiva.py
@@ -1,1 +1,13 @@
 # Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificio, indo de 10 até 0 com uma pausa de 1 segundo entre eles
+
+import time
+
+print('Vai iniciar a contagem regressiva, para os FOGOS!')
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+    if i == 1:
+        print('='*20)
+        print('FELIZ ANO NOVO!!!')
+        print('='*20)
