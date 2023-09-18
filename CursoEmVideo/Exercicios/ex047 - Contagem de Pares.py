f82b88ea95eb89f7696b3646ea2f3df0cc686f5a
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 12:15:47 -0300

    resolvendo ex047

--- CursoEmVideo/Exercicios/ex047 - Contagem de Pares.py
+++ CursoEmVideo/Exercicios/ex047 - Contagem de Pares.py
@@ -1,1 +1,9 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+
+print('-'*20)
+print('CONTAGEM DE NÚMEROS PARES')
+print('-'*20)
+
+for i in range(0, 51):
+    if (i % 2) == 0:
+        print(i)
