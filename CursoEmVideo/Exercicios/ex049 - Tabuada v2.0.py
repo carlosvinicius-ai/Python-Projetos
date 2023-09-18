09e4f384c56c0a157c889258fabebead17f0134a
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 14:45:38 -0300

    resolvendo ex049

--- CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
+++ CursoEmVideo/Exercicios/ex049 - Tabuada v2.0.py
@@ -1,1 +1,8 @@
 # Refaça o desafio 009, mostrando a tabuada de um numero que o usuário escolher, só que agora utilizando o laço for
+
+num1 = int(input('Escolha um número para ser multiplicado: '))
+num2 = int(input('Escolha até qual número será multiplicado: '))
+
+for i in range(num2):
+    print('{} X {} = {}'.format(num1, num2, num1*num2))
+    num2 -= 1
