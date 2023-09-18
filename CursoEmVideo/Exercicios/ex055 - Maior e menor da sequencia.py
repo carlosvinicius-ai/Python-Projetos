bd45efd388f4547a61362256164d457e0a49e10f
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 18:46:08 -0300

    resolvendo ex054

--- CursoEmVideo/Exercicios/ex055 - Maior e menor da sequencia.py
+++ CursoEmVideo/Exercicios/ex055 - Maior e menor da sequencia.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos
+
+peso_lista = []
+
+for i in range(1, 6):
+    peso = float(input('Digite o peso em KG da {}ª pessoa: '.format(i)))
+    peso_lista.append(peso)
+
+print('O maior peso registrado foi {:.1f}KG e o menor peso registrado foi {:.1f}KG'.format(max(peso_lista), min(peso_lista)))
