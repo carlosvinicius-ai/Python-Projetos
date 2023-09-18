1452105c50640e81e58206f70a85e015a68777ba
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-18 14:25:43 -0300

    resolvendo ex048

--- CursoEmVideo/Exercicios/ex048 - Soma impares multiplos de tres.py
+++ CursoEmVideo/Exercicios/ex048 - Soma impares multiplos de tres.py
@@ -1,6 +1,7 @@
 # Faça um programa que calcule a soma entre todos os número impares que são multiplos de três e que se encontram no intervalo de 1 até 500
 
+soma = 0
 for i in range (3, 500, 3):
     if (i%2) != 0:
-        print(i)
-print('ACABOU!')
+        soma += i
+print(soma)
