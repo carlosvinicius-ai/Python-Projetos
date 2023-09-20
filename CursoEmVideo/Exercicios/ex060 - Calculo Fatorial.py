2f85d4fc2eee48baad083b3e93d8ff92308ce51a
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-20 18:08:04 -0300

    resolvendo ex060

--- CursoEmVideo/Exercicios/ex060 - Calculo Fatorial.py
+++ CursoEmVideo/Exercicios/ex060 - Calculo Fatorial.py
@@ -2,3 +2,13 @@
 Faça um programa que leia um número qualquer e mostre seu fatorial
 Ex: 5! = 5x4x3x2x1 = 120
 '''
+
+n = int(input('Digite o número que deseja fatoriar: '))
+resultado = 1
+count = 1
+
+while count <= n:
+    resultado *= count
+    count += 1
+
+print(resultado)
