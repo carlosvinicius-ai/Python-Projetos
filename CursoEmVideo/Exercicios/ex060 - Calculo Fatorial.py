e9b8cd8baad2c492fab8e2d029a9081b504c836e
cvinicius-ai <cvinicius32@hotmail.com>
2023-09-21 19:49:01 -0300

    melhorando código

--- CursoEmVideo/Exercicios/ex060 - Calculo Fatorial.py
+++ CursoEmVideo/Exercicios/ex060 - Calculo Fatorial.py
@@ -3,12 +3,8 @@ Faça um programa que leia um número qualquer e mostre seu fatorial
 Ex: 5! = 5x4x3x2x1 = 120
 '''
 
-n = int(input('Digite o número que deseja fatoriar: '))
-resultado = 1
-count = 1
+from math import factorial
 
-while count <= n:
-    resultado *= count
-    count += 1
-
-print(resultado)
+n = int(input('Digite um número para clacular o seu Fatorial: '))
+f = factorial(n)
+print('O fatorial de {}! é {}'.format(n, f))
