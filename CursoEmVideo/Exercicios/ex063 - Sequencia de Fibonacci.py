22b0c27c639eff7d67383e2a65d77ac45df56bb0
cvinicius-ai <cvinicius32@hotmail.com>
2024-02-06 17:52:57 -0300

    add ex063

--- CursoEmVideo/Exercicios/ex063 - Sequencia de Fibonacci.py
+++ CursoEmVideo/Exercicios/ex063 - Sequencia de Fibonacci.py
@@ -1,3 +1,26 @@
 '''
 escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma sequencia de fibonacci
 '''
+
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+
+n = int(input('Quantos termos você quer mostrar: '))
+
+t1 = 0
+t2 = 1
+print('-'*30)
+print(f'{t1} -> {t2}', end='')
+
+cont = 3
+
+while cont <= n:
+    t3 = t1 + t2
+    print(f' -> {t3}', end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+    
+print(' -> FIM')
+print('-'*30)
