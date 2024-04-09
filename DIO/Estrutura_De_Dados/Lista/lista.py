c05ca5ff4b51d932370cf167c18d6f1b8db17efd
carlosvinicius-ai <cvinicius32@hotmail.com>
2024-04-09 11:14:31 -0300

    add lista

---
+++ DIO/Estrutura_De_Dados/Lista/lista.py
@@ -0,0 +1,45 @@
+# como declara lista
+
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+# como adicionar informação na lista
+
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+
+# como limpar a lista
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
+
+# como copiar a lista
+
+lista = [1, "Python", [40, 30, 20]]
+
+lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
