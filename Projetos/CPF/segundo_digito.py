a97af2fa292899c439159254ab9c8869668f5329
carlosvinicius-ai <cvinicius32@hotmail.com>
2025-03-11 18:03:02 -0300

    att with regex

--- Projetos/CPF/segundo_digito.py
+++ Projetos/CPF/segundo_digito.py
@@ -1,59 +1,50 @@
-cpf = input('Digite seu cpf: ')
+import re
+import sys
 
-cpf_separado = cpf.split('-') # separando os primeiros 9 digitos
-nove_digitos = []
+# cpf_enviado_usuario = '746.824.890-70' \
+#     .replace('.', '') \
+#     .replace(' ', '') \
+#     .replace('-', '')
+entrada = input('CPF: ')
+cpf_enviado_usuario = re.sub(
+    r'[^0-9]',
+    '',
+    entrada
+) # retornando uma expressão regular
 
-for numero in cpf_separado[0]:
-    try:
-        numero = int(numero)
-        nove_digitos.append(numero) # armazenando os números para calculo
-    except ValueError:
-        ...
-
-contador = 10
-soma_nove_digitos = 0
-for numero in nove_digitos:
-    soma_nove_digitos += numero * contador 
-    contador -= 1
-
-# multiplicando a soma do resultado por 10 e trazendo o resto de 11
-primeiro_numero_cpf_calculo = (soma_nove_digitos * 10) % 11 
-
-resultado_primeiro_digito = primeiro_numero_cpf_calculo if primeiro_numero_cpf_calculo <= 9 else 0
+entrada_e_sequencial = entrada == entrada[0] * len(entrada)
 
 
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
 
-# Segundo digito
-dez_digitos = nove_digitos.copy()
-dez_digitos.append(resultado_primeiro_digito)
+# primeiro digito
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
 
-contador = 11
-resultado = 0
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
 
+# segundo digito
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
 for digito in dez_digitos:
-    resultado += int(digito) * contador
-    contador -= 1
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
 
-digito = (resultado * 10) % 11
-segundo_digito = digito if digito <= 9 else 0
+# verificando se é válido ou não
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-
-# verificação
-dez_digitos.append(segundo_digito)
-novo_cpf = ''
-# transformando a lista em texto para poder fazer a verificação da forma correta
-for i, digito in enumerate(dez_digitos):
-    if i == 2 or i == 5:
-        novo_cpf += str(digito)
-        novo_cpf += '.'
-        continue
-    elif i == 8:
-        novo_cpf += str(digito)
-        novo_cpf += '-'
-        continue
-    novo_cpf += str(digito)
-
-if cpf == novo_cpf:
-    print(f'{cpf} enviado é VÁLIDO')
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido')
 else:
-    print('CPF INVÁLIDO')
+    print('CPF inválido')
